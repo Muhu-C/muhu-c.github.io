b8a5f773791425dd4d6813a0ab4ce9901d03bc04
Muhu-C <452044217@qq.com>
2025-07-04 17:16:26 +0800

    Site updated: 2025-07-04 17:16:25

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/04/如何建立自己的博客(旧博客)/","2025/07/04/木沪时间表2-0-0-0更新了！/"];function toRandomPost(){
+var posts=["2025/07/04/木沪时间表2-0-0-0更新了！/","2024/06/04/如何建立自己的博客(旧博客)/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
